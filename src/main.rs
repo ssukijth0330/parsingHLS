47cc63ca4e720b0b9cffa06198088319d3d7ed91
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-16 16:26:49 -0400

    add comment in main functioin

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@ mod Duration;
 fn main() {
 
     let url = "https://docs.evostream.com/sample_content/assets/hls-bunny-rangerequest/bunny/playlist.m3u8";
+    //Display the content of the m3u8 file
     support::display_content_of_m3u8_url(url);
+    //Calculate the duration of the whole video
     Duration::from_secs_f32(url);
 
 }
