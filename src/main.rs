24cbc8dc230444cfaf4b5d0348635c3ac6504eb1
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-20 23:00:40 -0400

    testing

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod duration;
 
 fn main() {
 
-    let url = "https://docs.evostream.com/sample_content/assets/hls-bunny-rangerequest/bunny/playlist.m3u8";
+    //let url = "https://docs.evostream.com/sample_content/assets/hls-bunny-rangerequest/bunny/playlist.m3u8";
     // let url = "http://localhost/hls/bigb_720.m3u8";
     //let url = "http://localhost/hls/bigb_480.m3u8";
     let url = "https://lw.bamgrid.com/2.0/hls/vod/bam/ms02/hls/dplus/bao/master_unenc_avc_aac_subs_ccjk.m3u8";
