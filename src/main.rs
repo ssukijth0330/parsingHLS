36911596a9da410153f4e8bec14c3611362466da
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-20 22:40:41 -0400

    testing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     // let url = "http://localhost/hls/bigb_720.m3u8";
     //let url = "http://localhost/hls/bigb_480.m3u8";
     //let url = "https://lw.bamgrid.com/2.0/hls/vod/bam/ms02/hls/dplus/bao/master_unenc_avc_aac_subs_ccjk.m3u8";
-    let url = "http://localhost/hls/sintel/sintel_4k.m3u8";
+    //let url = "http://localhost/hls/sintel/sintel_4k.m3u8";
     println!("URL: {}\n", url);
     // Display the content of the m3u8 file
     // duration::display_content_of_m3u8_url(url);
