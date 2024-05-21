6ac205abd7ccd6b604a7966c8c715ccb59748e2c
Suttipong (Sutti) Sukijthamapan <ssukijth@gmail.com>
2024-05-20 22:30:28 -0400

    Add second layer of m3u8

--- src/Duration.rs
+++ src/Duration.rs
@@ -1,19 +1,20 @@
 use core::{f32, str};
 use reqwest::blocking::get;
 
-//function to parse the HLS m3u8 file to get duration by using Duration::from_secs_f32
+//function to parse the HLS m3u8 file to get duration of each segment, 
+//then sum the duration of each segment to get the total duration of the video
 // Input m3u8 URL
-// Output print the duration of the video
+// Output print the duration of the total duration of the video
 pub fn from_secs_f32(url: &str) {
-    //read the url string. 
+    //read the url from user input string. 
     let response = get(url)
         .expect("Failed to read the URL")
         .text()
         .expect("Failed to read the URL");
-    //split the file by new line
+    //Brake the m3u8 file into vector, line by line
     let lines: Vec<&str> = response.split("\n").collect();
     let mut duration:f32 = 0.0;
-    // To calulate the duration of the entire video in the manifest
+    // Combine the duration of segments in the manifest:
     // The code needs to parse the #EXTINF tag and sum the duration of each segment
     // such as (sec)
     // #EXTINF:10.00,
@@ -24,31 +25,126 @@ pub fn from_secs_f32(url: &str) {
     // So the total duration of the video is 10.00 + 12.01 = 22.01 sec
     // The code will print the total duration of the video
     for line in lines {
+        //focus on the line that contains the #EXTINF tag for duration
         if line.contains("#EXTINF") {
-            //let duration_str = line.split(":").collect::<Vec<&str>>()[1];
+            //split the line by ":"
             let duration_vec: Vec<&str> = line.split(":").collect();
-            //get the duration but it is in a string format
+            //got the duration in duration_vec[1]
             // but it may contain unwanted characters
+            // convert the duration to string
             let duration_f32_str_raw = duration_vec[1].to_string();
             // trim the string
             let duration_f32_str_raw = duration_f32_str_raw.trim();
             // There is a "," at the end of the duration string
             // remove the "," from the string
-            let remove_char = ',';
-            // remove the remove_char from the string
-            let duration_f32_str: String = remove_char_from_string(&duration_f32_str_raw, remove_char);
-
-            // convert the string to f32 format
-            let duration_f32: f32 = duration_f32_str.parse().unwrap();
+            //split the string by ","
+            let duration_f32_vec: Vec<&str> = duration_f32_str_raw.split(",").collect();
+            // The duration is in duration_f32_vec[0]
+            // then convert the string to f32 format
+            let duration_f32: f32 = duration_f32_vec[0].parse().unwrap();
             //sum the duration of each segment
             duration = duration + duration_f32;
+            //debug print the duration of each segment
+            //print!("{} + ", duration_f32);
+
         }
     }
-    println!("The duration to play all video segments in this file is: {}", duration
-);
+    println!("The duration to play all segments of this video is(secs): {}\n", duration);
+    // println!("Note:
+    // The duration may not accurate, because in the adative Bitrate Streaming, 
+    // we can not guarantee bandwidth of traffic, so the video segments from a different bandwidth may be use to display, 
+    // but the duration that shown above is calculated from all segments of the same bandwith m3u8 file.
+    
+    // To solve this problem, we need to ensure that each video segment has the same duration, 
+    // for every bandwidth.");
 }
 
-//function remove all characters from a string
-pub fn remove_char_from_string(s: &str, c: char) -> String {
-    s.chars().filter(|&x| x != c).collect()
+//function to parse the HLS m3u8 file to get duration of each segment:
+//
+// Two level of m3u8 files: (found #EXT-X-STREAM-INF tag in the file)
+//Read URL of m3u8 and parse manifest in m3u8 find the EXT-X-STREAM-INF tag and follow the file
+// Input m3u8 URL
+// Output string with the EXT-X-STREAM-INF tag
+// Example:
+// #EXT3U
+// #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=232370,CODECS="mp4a.40.2,avc1.4d4015",RESOLUTION=640x480
+// bigb_480.m3u8
+// #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=649879,CODECS="mp4a.40.2,avc1.4d4015",RESOLUTION=1024x720
+// bigb_720.m3u8
+//
+// The code will print the duration of bigb_480.m3u8 and bigb_720.m3u8
+//
+// Single level of m3u8 files: (found the #EXTINF tag in the file)
+//  call -> from_secs_f32(url);
+//
+pub fn parse_m3u8_url(url: &str) {
+    //display_content_of_m3u8_url(url);
+    //read content from the url.
+    let file = get(url)
+        .expect("Failed to read the URL")
+        .text()
+        .expect("Failed to read the URL");
+
+    //split the content line-by-line put into vector
+    let lines: Vec<&str> = file.split("\n").collect();
+    let mut read_inside_m3u8 = false;
+    for line in lines {
+        if read_inside_m3u8 { // Prevoiuse line contains #EXT-X-STREAM-INF
+            read_inside_m3u8 = false;
+            // got the file name inside the m3u8 file
+            // Then combine the base URL with the file name from inside the m3u8 file
+            let url = replace_with_newfilename_in_url(url.trim(), line.trim());
+
+            // Display the content of the m3u8 file
+            //display_content_of_m3u8_url(&url);
+
+            // Calculate the duration of the whole video
+            println!("Calculate duration of {}", url);
+            from_secs_f32(&url);
+        }
+        match line.to_string() {
+            s if s.contains("#EXT-X-STREAM-INF") => {
+                // Second layer of m3u8:
+                // if found m3u8 inside m3u8
+                // read the next line to get the second layer m3u8 file name
+                read_inside_m3u8 = true;
+                continue;
+            },
+            s if s.contains("#EXTINF") => {
+                // Single layer of m3u8:
+                // No #EXT-X-STREAM-INF tag in the file, 
+                // So it contains only one layer of m3u8 file
+                // then calculate the duration of the video
+                from_secs_f32(url);
+                break;
+            },
+            _ => { continue; }
+
+         }
+    }
+}
+
+//funcion to combine the base URL with the file name that we found in the m3u8 file
+//Input: the original URL and the file name that we found in the m3u8 file
+//Output: the combined URL
+pub fn replace_with_newfilename_in_url(in_url: &str, m3u8_filename: &str) -> String {
+    //get the file name from URL
+    let file_name = in_url.split('/').last().unwrap();
+    //remove the file name from URL
+    let url_no_file = in_url.replace(file_name, "");
+    //combine the base URL with path and the file name that we found in the m3u8 file
+    let url = format!("{}{}", url_no_file, m3u8_filename);
+    url
+}
+
+//Read URL and print the content
+pub fn display_content_of_m3u8_url(url: &str) {
+    println!("Display the content of m3u8 file:, {}", url);
+    //read the url string. 
+    let response = get(url)
+        .expect("Failed to read the URL")
+        .text()
+        .expect("Failed to read the URL");
+    //print the response
+    println!("{}", response);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-mod support;
-mod Duration;
+mod duration;
 
 fn main() {
 
     let url = "https://docs.evostream.com/sample_content/assets/hls-bunny-rangerequest/bunny/playlist.m3u8";
-    //Display the content of the m3u8 file
-    support::display_content_of_m3u8_url(url);
-    //Calculate the duration of the whole video
-    Duration::from_secs_f32(url);
-
+    // let url = "http://localhost/hls/bigb_720.m3u8";
+    //let url = "http://localhost/hls/bigb_480.m3u8";
+    //let url = "https://lw.bamgrid.com/2.0/hls/vod/bam/ms02/hls/dplus/bao/master_unenc_avc_aac_subs_ccjk.m3u8";
+    let url = "http://localhost/hls/sintel/sintel_4k.m3u8";
+    println!("URL: {}\n", url);
+    // Display the content of the m3u8 file
+    // duration::display_content_of_m3u8_url(url);
+    // Calculate the duration of the whole video
+    duration::parse_m3u8_url(url);
 }
